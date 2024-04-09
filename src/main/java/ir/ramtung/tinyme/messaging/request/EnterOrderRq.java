d6cd26b5c3f189ac424134c25234d29024eadaa6
Fereshte <fereshte12bagheri@gmail.com>
2024-04-09 11:10:42 +0330

    add new constructors for iceberge and enterOrderReq

--- src/main/java/ir/ramtung/tinyme/messaging/request/EnterOrderRq.java
+++ src/main/java/ir/ramtung/tinyme/messaging/request/EnterOrderRq.java
@@ -51,4 +51,12 @@ public class EnterOrderRq {
         return new EnterOrderRq(OrderEntryType.UPDATE_ORDER, requestId, securityIsin, orderId, entryTime, side, quantity, price, brokerId, shareholderId, peakSize, minimumExecutionQuantity);
     }
 
+    public static EnterOrderRq createNewOrderRq(long requestId, String securityIsin, long orderId, LocalDateTime entryTime, Side side, int quantity, int price, long brokerId, long shareholderId, int peakSize) {
+        return new EnterOrderRq(OrderEntryType.NEW_ORDER, requestId, securityIsin, orderId, entryTime, side, quantity, price, brokerId, shareholderId, peakSize, 0);
+    }
+
+    public static EnterOrderRq createUpdateOrderRq(long requestId, String securityIsin, long orderId, LocalDateTime entryTime, Side side, int quantity, int price, long brokerId, long shareholderId, int peakSize) {
+        return new EnterOrderRq(OrderEntryType.UPDATE_ORDER, requestId, securityIsin, orderId, entryTime, side, quantity, price, brokerId, shareholderId, peakSize, 0);
+    }
+
 }
