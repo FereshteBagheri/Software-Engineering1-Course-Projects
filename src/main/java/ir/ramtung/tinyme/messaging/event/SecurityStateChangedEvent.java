c9dd7ed30414571320bdf013e91a4f02a8f2a4c3
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-05-08 15:54:01 +0330

    add security state change event

---
+++ src/main/java/ir/ramtung/tinyme/messaging/event/SecurityStateChangedEvent.java
@@ -0,0 +1,18 @@
+package ir.ramtung.tinyme.messaging.event;
+
+import ir.ramtung.tinyme.messaging.request.MatchingState;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+public class SecurityStateChangedEvent extends Event{
+    private LocalDateTime time;
+    private String securityIsin;
+    private MatchingState state;
+}
