3a71dc004d49b1af8d0723eeea34c291c0f001c7
PishbinZein <132846420+PishbinZein@users.noreply.github.com>
2024-06-01 01:39:32 +0330

    Revert "refactor: edit variable names in customPair class"
    This reverts commit c164e23cc98be0a94661ef822ded83188e000254.

--- src/main/java/ir/ramtung/tinyme/domain/entity/CustomPair.java
+++ src/main/java/ir/ramtung/tinyme/domain/entity/CustomPair.java
@@ -3,17 +3,14 @@ package ir.ramtung.tinyme.domain.entity;
 public class CustomPair {
     private int first;
     private int second;
-
-    public CustomPair(int first, int second) {
-        this.first = first;
-        this.second = second;
+    public CustomPair(int firstValue, int secondValue){  
+        this.first = firstValue;  
+        this.second = secondValue; 
     }
-
-    public int getFirst() {
-        return first;
+    public int getFirst(){
+        return this.first;
     }
-
-    public int getSecond() {
-        return second;
+    public int getSecond(){
+        return this.second;
     }
 }
